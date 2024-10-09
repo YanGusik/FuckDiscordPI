32f65b65ba4afdefde8f141611e71ea60e849944
YanGus <minibaev2001@yandex.kz>
2024-10-09 20:22:35 +0400

    update mgts and update scripts alternative

--- discord_alternative_three.cmd
+++ discord_alternative_three.cmd
@@ -1,5 +1,5 @@
 start "FuckDiscordPi: Discord (YanGusik)" /min "%~dp0winws.exe" ^
 --wf-tcp=443-65535 --wf-udp=443-65535 ^
---filter-udp=443 --hostlist="%~dp0list-discord.txt" --dpi-desync=split2 --dpi-desync-split-pos=2 --wssize 1:8 --hostnospace--dpi-desync-udplen-increment=10 --dpi-desync-repeats=6 --dpi-desync-udplen-pattern=0xDEADBEEF --dpi-desync-fake-quic="%~dp0quic_initial_www_google_com.bin" --new ^
+--filter-udp=443 --hostlist="%~dp0list-discord.txt" --dpi-desync=split2 --dpi-desync-split-pos=2 --wssize 1:8 --hostnospace --dpi-desync-udplen-increment=10 --dpi-desync-repeats=6 --dpi-desync-udplen-pattern=0xDEADBEEF --dpi-desync-fake-quic="%~dp0quic_initial_www_google_com.bin" --new ^
 --filter-udp=50000-65535 --dpi-desync=split2 --dpi-desync-split-pos=2 --wssize 1:8 --dpi-desync-any-protocol --dpi-desync-fake-quic="%~dp0quic_initial_www_google_com.bin" --new ^
 --filter-tcp=443 --hostlist="%~dp0list-discord.txt" --dpi-desync=split2 --dpi-desync-split-pos=2 --wssize 1:8 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls="%~dp0tls_clienthello_www_google_com.bin"
